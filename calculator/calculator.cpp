2cd7a556b64101f7ae5d77647d1aa1b9826ad746
Misha Ivanov <misha.ivanov.ig@gmail.com>
2018-11-25 18:24:07 +0300

    Add calculator
    Signed-off-by: Misha Ivanov <misha.ivanov.ig@gmail.com>

---
+++ calculator/calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int number1;
+int number2;
+int number3;
+cout<<"Press the first  number :";
+cin>>number1;
+char t;
+cout <<"Press operation(+,-,*,/):";
+cin >> t;
+cout<<"Press the second number:";
+cin>>number2;
+if (t == '+')
+{
+number3 = number1 + number2;
+cout<<"Result; "<<number3<<endl;
+}
+if (t == '-')
+{
+number3 = number1 - number2;
+cout<<"Result: "<< number3 << endl;
+}
+if (t == '*')
+{
+number3 = number1 * number2;
+cout<<"Result: "<<number3<<endl;
+}
+if (t == '/')
+{
+    if (number2 == 0) {
+        cout<<"0 can't be here!"<<endl;
+    } else {
+        number3 = number1 / number2;
+        cout<<"Result: " <<number3<<endl;
+    }
+}
+}
+ 
